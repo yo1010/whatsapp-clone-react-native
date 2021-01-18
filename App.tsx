039a4cd1b1b5ea26a710f73eea87ee1c677360fe
yavord10 <yavor.v.dimitrov@gmail.com>
2021-01-18 12:19:25 +0800

    add auth to app

--- App.tsx
+++ App.tsx
@@ -1,19 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-import Amplify from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 import config from './aws-exports';
 
 Amplify.configure(config)
 
-export default function App() {
+const App = () => {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      //get auth user from auth 
+      const auth = await Auth.currentAuthenticatedUser({ bypassCache: true });
+
+      console.log(auth)
+      //get user from BE with id from auth
+
+      //if no user in db --> create one
+    };
+
+    fetchUser();
+  }, [])
 
   if (!isLoadingComplete) {
     return null;
@@ -25,4 +40,6 @@ export default function App() {
       </SafeAreaProvider>
     );
   }
-}
+};
+
+export default withAuthenticator(App);
