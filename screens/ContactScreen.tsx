1de64cd48126db8fd46e76d8217afd381029f86a
yavord10 <yavor.v.dimitrov@gmail.com>
2021-01-22 12:37:17 +0800

    fetch users from database

--- screens/ContactScreen.tsx
+++ screens/ContactScreen.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { API, graphqlOperation } from 'aws-amplify';
+import React, { useEffect, useState } from 'react'
 import { FlatList, StyleSheet, View} from 'react-native';
 import ContactListItem from '../components/ContactListItem';
-import users from '../Users';
+import { listUsers } from '../graphql/queries';
 
 const ContactScreen = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const userData = await API.graphql(
+                    graphqlOperation(
+                        listUsers
+                    )
+                );
+                setUsers(userData.data.listUsers.items);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fetchUsers();
+    }, []);
+
     return (
         <View style={styles.container}>
             <FlatList
